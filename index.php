c77ebac633595c92eab2450d62398120516eb8d4
Rodrick Gordon <rodrick_gordon@yahoo.com>
2021-12-22 23:59:46 -0500

    Index Added

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    $title='DTU Home';
+    require_once 'includes/header.php';
+    require_once 'db/conn.php';
+ ?>
+
+
+    
+</div>
+
+
+
+   
+    
+    <?php require_once 'includes/footer.php'; ?>
