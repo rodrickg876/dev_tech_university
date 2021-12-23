4eb017f257da7aa95829070ad09f809af6c69972
Rodrick Gordon <rodrick_gordon@yahoo.com>
2021-12-23 01:46:24 -0500

    updated about us

--- aboutus.php
+++ aboutus.php
@@ -3,9 +3,29 @@
     require_once 'includes/header.php';
 ?>
 
-<div class="p-5 text-center">
-    <h1>Find your programme </h1>
-        <p>Choose from over 200 programmes across 7 faculties and 50 departments and research centres.</p>
+<div class="row " id="about">
+    <div class="col-6">
+        <h1 class="hd1 text-white">About<br> Dev Tech<br> University</h1>
+    </div>
+    
+    <div class="col-6">
+        <p class="hd2 text-white"> 
+        DTU is at the frontier of academic and intellectual discovery. 
+        Those who venture here—to learn, research, teach, work, and grow—join 
+        nearly four centuries of students and scholars in the pursuit of truth, 
+        knowledge, and a better world.
+        </p>
+    </div>
 </div>
+<div class="row p-5 text-center">
+    <div><h4><i><u>HIGHER EDUCATION, HIGHER PURPOSE</u></i></h4></div>
+<div class="col-6 offset-3"> 
+    <p class="hd2">   
+As a research university and nonprofit institution, DTU is focused on creating educational
+ opportunities for people from many lived experiences.
+    </p>
+</div>
+</div>
+   
 
 <?php require_once 'includes/footer.php'; ?>
