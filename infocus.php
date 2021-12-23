0e1e5203fcd93e49dec860d49e483e95d921cb46
Rodrick Gordon <rodrick_gordon@yahoo.com>
2021-12-23 02:26:19 -0500

    added infocus page and images

--- infocus.php
+++ infocus.php
@@ -2,7 +2,62 @@
     $title='In Focus';
     require_once 'includes/header.php';
 ?>
-
+<div class="row " id="about">
+    <div class="col-6">
+        <h1 class="hd1 text-white">In Focus</h1>
+    </div>
+    
+    <div class="col-6">
+        <p class="hd2 text-white"> 
+        In Focus is a window into the ideas, research, 
+        and work happening across Harvard communities. Each
+         week we bring a different topic into focus,
+         sharing perspectives on the big conversations 
+         of the moment—and sparking new ones.
+        </p>
+    </div>
+</div>
+<br><br>
+<div class="row col-8 offset-2">
+    <div class="col-7">
+    <img src="images/focus2.jpg" class="img-fluid img img-thumbnail" />
+    </div>
+    <div class="col-5">
+        <p class="hd3">
+        The global pandemic changed the way we work and how our workplaces function. 
+        Our experts investigate what changes may be here 
+        to stay and what else to expect in the future.
+        </p>
+    </div>
+</div>
+<br>
+<div class="row col-8 offset-2">
+    <div class="col-5">
+    <p class="hd3">
+    A school maintenance programme is an organizational activity carried out
+     by the school community in order to prolong the life expectancy of school buildings, 
+     its furniture and equipment.
+    </p>
+    </div>
+    <div class="col-7">
+    <img src="images/focus3.jpg" class="img-fluid img img-thumbnail" />
+    </div>
+</div>
+<br>
+<div class="row col-8 offset-2">
+    <div class="col-7">
+    <img src="images/focus1.jpg" class="img-fluid img img-thumbnail" />
+    </div>
+    <div class="col-5">
+    <p class="hd3">
+    Despite being rattled by the Covid-19 pandemic we still persevered. 
+    Our annual graduation ceremony was a huge succes. We look forward to 
+    the continued support and service of out beloved friends and familiy.
+    <br> <strong><i>All the best for 2022!</i></strong>
+</p>
+    </div>
+</div>
+<br>
 
 
 <?php require_once 'includes/footer.php'; ?>
