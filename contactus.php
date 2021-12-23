f24b79737c1fff20a17cce11cc1c9dfd55cf404d
Rodrick Gordon <rodrick_gordon@yahoo.com>
2021-12-23 02:42:05 -0500

    inserted contact us

--- contactus.php
+++ contactus.php
@@ -3,6 +3,43 @@
     require_once 'includes/header.php';
 ?>
 
+<br>
+<div class="row p-5 ">
+    <div class="col-7">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6671.802674686112!2d-77.91110000000002!3d18.469990000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd9a047e8124fd91c!2sNorthern%20Caribbean%20University%20-%20Montego%20Bay%2C%20Western%20Campus!5e1!3m2!1sen!2sjm!4v1640244555468!5m2!1sen!2sjm" 
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div class="col-5">
 
+    <ul class="info1"><b>Address:</b>
+        <li>1 Lime Tree Lane, Mandeville</li> 
+    </ul>
+    <ul class="info1"><b>Telephone:</b>
+        <li>876-887-9876</li>
+        <li>876-352-7721</li>   
+    </ul>
+    <ul class="info1"><b>Email:</b>
+        <li>info@dtumand.edu.jm</li> 
+    </ul>
+    </div>
+</div>
+<div class="row p-4 ">
+    <div class="col-5">
+    <ul class="info1"><b>Address:</b>
+        <li>6 Gordon Town Road, Kingston</li> 
+    </ul>
+    <ul class="info1"><b>Telephone:</b>
+        <li>876-926-0092</li>
+        <li>876-963-9912</li>   
+    </ul>
+    <ul class="info1"><b>Email:</b>
+        <li>info@dtukgn.edu.jm</li> 
+    </ul>
+    </div>
+    <div class="col-7">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d789.1896374164857!2d-76.7401916306985!3d18.024216342255738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8edb3ed185ee64f7%3A0xeb79a64e1f8b38a!2sVTDI%20HEART%2C%20Kingston!5e1!3m2!1sen!2sjm!4v1640244660334!5m2!1sen!2sjm" 
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+</div>
 
 <?php require_once 'includes/footer.php'; ?>
