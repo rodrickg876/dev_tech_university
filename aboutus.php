f23e4f24c10672be57ec5edeae70da7ac678cf2b
Rodrick Gordon <rodrick_gordon@yahoo.com>
2021-12-23 01:20:31 -0500

    about

--- aboutus.php
+++ aboutus.php
@@ -3,6 +3,9 @@
     require_once 'includes/header.php';
 ?>
 
-
+<div class="p-5 text-center">
+    <h1>Find your programme </h1>
+        <p>Choose from over 200 programmes across 7 faculties and 50 departments and research centres.</p>
+</div>
 
 <?php require_once 'includes/footer.php'; ?>
